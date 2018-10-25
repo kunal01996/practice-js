0718c3c3324982f156307bd50412a3706683d507
MayankG4 <mayankgupta0589@gmail.com>
2018-10-25 10:07:14 +0530

    Update helloworld.js

--- basics/intro/helloworld.js
+++ basics/intro/helloworld.js
@@ -5,8 +5,8 @@
  (function(){
 
     /**Code starts here */
-
-    "use strict"
+//You can use strict mode in all your programs. It helps you to write cleaner code, like preventing you from using undeclared variables. 
+    "use strict";
     function greet(name) {
         console.log(`your name is ${name}`);
     }
